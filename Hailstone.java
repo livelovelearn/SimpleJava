7a08518fbbfa2af6c71d0887086bf984f39308a0
S <s>
2013-08-02 19:57:43 -0400

    {"type":"TI", "runs":"2"}

--- Hailstone.java
+++ Hailstone.java
@@ -10,7 +10,26 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		int num = readInt("Enter a number");
+		int count=0;
+		int num2;
+		while(num!=1)
+			{count++;
+			if (num%2==1)
+		     {
+			num2=3*num+1;
+			println(num+" is odd, so I make 3n + 1: "+ num2);
+			num=num2;
+		     }
+		    else
+		     {
+			num2=num/2;
+			println(num+" is even, so I take half: "+ num2);
+			num=num2;
+		     }
+		    }
+		println("The process took " + count +" to reach 1" );
+		
 	}
 }
 
