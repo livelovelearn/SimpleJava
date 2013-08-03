ccff5817ca2cae55f14ce8a3e843c597cfaece58
S <s>
2013-08-02 20:28:40 -0400

    {"type":"TI", "runs":"3"}

--- Target.java
+++ Target.java
@@ -12,7 +12,7 @@ import java.awt.*;
 
 public class Target extends GraphicsProgram {	
 	public void run() {
-		GOval oval1 = new GOval(getWidth()/2, getHeight()/2, 72, 72);
+		GOval oval1 = new GOval(getWidth()/2-36, getHeight()/2-36, 72, 72);
 		oval1.setFilled(true);
 		oval1.setColor(Color.RED);
 		add (oval1);
