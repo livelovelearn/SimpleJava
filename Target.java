59cf0008ceb11343652069b87a459068e92e89d8
Yancheng Liu <morningsherry@hotmail.com>
2013-10-27 14:28:31 -0400

    Update Target.java

--- Target.java
+++ Target.java
@@ -1,10 +1,4 @@
-/*
- * File: Target.java
- * Name:
- * Section Leader:
- * -----------------
- * This file is the starter file for the Target problem.
- */
+// This program draws an image of archery target.
 
 import acm.graphics.*;
 import acm.program.*;
@@ -16,15 +10,18 @@ public class Target extends GraphicsProgram {
 		oval1.setFilled(true);
 		oval1.setColor(Color.RED);
 		add (oval1);
+		//draw the biggest circle at the bottom
 		
 		GOval oval2 = new GOval(getWidth()/2-23, getHeight()/2-23, 46, 46);
 		oval2.setFilled(true);
 		oval2.setColor(Color.white);
 		add (oval2);
+		//draw the second circle on top of the first
 		
 		GOval oval3 = new GOval(getWidth()/2-11, getHeight()/2-11, 22, 22);
 		oval3.setFilled(true);
 		oval3.setColor(Color.RED);
 		add (oval3);
+		//draw the third circle on top of the second
 	}
 }
